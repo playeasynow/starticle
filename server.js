6f8a29491758917fe078f3407d1612fcba71d350
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-12-23 22:32:59 -0600

    cleaned up server.js

--- server.js
+++ server.js
@@ -49,21 +49,15 @@ app.get("/scrape", function(req, res) {
       result.summary = $(this).siblings().children().children().text();
       result.imageURL = "https://medium.com" + $(this).parent().parent().parent().siblings().children().children().attr("href");
 
-      console.log(result, "This ----------------------------------")
-
       // creates new article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, log it
           console.log(err);
         });
     });
-
-    // Send a message to the client
     res.send("scrape complete");
   });
 });
@@ -73,49 +67,43 @@ app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
+// Route for grabbing a specific Article by id and populating it with its note
 app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  // Using the id passed in the id parameter, query the matching one in our db
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
+    // populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
+  // create new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Start the server
+// start server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
